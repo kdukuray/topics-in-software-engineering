e82b34d0b06b2e334990f9f7c9b9edbfcd87f141
Alexandr Voronovich <120990499+gizmo1547@users.noreply.github.com>
2025-03-12 20:31:47 -0400

    Update models.py

--- LedgerPay/models.py
+++ LedgerPay/models.py
@@ -1,17 +1,12 @@
 #models.py
-from sqlalchemy import Boolean, Column, Integer, String
+#For sqlite3
+from sqlalchemy import Column, Integer, String
 from database import Base
 
+
+#user
 class User(Base):
-    __tablename__ = 'users'  # Corrected typo: '__tablename__'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True)
-
-class Post(Base):
-    __tablename__ = 'posts'
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(50))
-    content = Column(String(100))  # Removed redundant String(String(...))
-    user_id = Column(Integer)
+    username = Column(String(50), unique=True, index=True)  # Added index for faster search
