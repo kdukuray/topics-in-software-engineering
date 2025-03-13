a1ba4901f87aa0c2d359dc3351590614374c77a0
Alexandr Voronovich <120990499+gizmo1547@users.noreply.github.com>
2025-03-12 21:56:33 -0400

    Update database.py

--- LedgerPay/database.py
+++ LedgerPay/database.py
@@ -1,18 +1,17 @@
 #database.py
 #For sqlite3
-#For sqlite3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./books.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./ledgerpay.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base() 
 '''
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
