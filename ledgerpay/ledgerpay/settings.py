f7e795395ca93e21b50e6f38b361c2e12ac22f4b
gizmo1547 <120990499+gizmo1547@users.noreply.github.com>
2025-04-04 18:47:18 -0400

    Push udated withdrawal function with withdrawal_test.html page

--- ledgerpay/ledgerpay/settings.py
+++ ledgerpay/ledgerpay/settings.py
@@ -117,10 +117,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+'''
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # The location of the static directory
 ]
+'''
+STATICFILES_DIRS = [
+    BASE_DIR / "payments" / "static"
+]
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
