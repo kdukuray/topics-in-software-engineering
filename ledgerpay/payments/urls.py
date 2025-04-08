7d1c0da094372e44943aa36449660cc4da0ec43a
kdukuray <kdukuray01@gmail.com>
2025-04-07 21:38:07 -0400

    update for demo

--- ledgerpay/payments/urls.py
+++ ledgerpay/payments/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import dashboard, signup
-from .views import withdraw_funds  # Import the new withdrawal function
+from .views import withdraw_funds, create_transaction  # Import the new withdrawal function
 
 urlpatterns = [
+    path("create-transaction/", create_transaction, name="create_transaction"),
     path("login/", LoginView.as_view(template_name="payments/login.html"), name="login"),
     path("", dashboard, name="dashboard"),
     path("logout", LogoutView.as_view(), name="logout"),
     path('signup/', signup, name='signup'),
-    path("withdraw/", withdraw_funds, name="withdraw"),  # Define a URL route for withdrawal
+    path("withdraw/", withdraw_funds, name="withdraw"),
+    # path("create-transaction/", create_transaction, name="create_transaction"),
+    # Define a URL route for withdrawal
 ]
