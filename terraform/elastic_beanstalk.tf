f90473d5a79dc5a51596c0b636f24fe099873559
terraform-fmt <noreply@corise.com>
2025-04-28 23:22:34 +0000

    fixup: terraform fmt

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -5,7 +5,7 @@ resource "aws_elastic_beanstalk_application" "django_app" {
 
 resource "aws_elastic_beanstalk_environment" "django_env" {
   name                = "ledgerpay-env"
-  cname_prefix        =  "kdukurayledgerpayassignment3"
+  cname_prefix        = "kdukurayledgerpayassignment3"
   application         = aws_elastic_beanstalk_application.django_app.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.5.1 running Python 3.11"
 
