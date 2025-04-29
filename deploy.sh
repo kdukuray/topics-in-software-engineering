2d83f156bb1e0b7618d25517c045036ae056ec3a
Adama Faye <46406662+adamafaye1945@users.noreply.github.com>
2025-04-29 15:45:49 -0400

    assignment completed

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-zip -r "flaskbb_deploy-$1.zip" ./flaskbb ./.ebextensions wsgi.py setup.py setup.cfg requirements.txt flaskbb.cfg celery_worker.py
+zip -r "flaskbb_deploy-081273d-20250429145730.zip" ./flaskbb ./.ebextensions wsgi.py setup.py setup.cfg requirements.txt flaskbb.cfg celery_worker.py
 
-aws s3 cp "flaskbb_deploy-$1.zip" s3://flaskbbapp
+aws s3 cp "flaskbb_deploy-081273d-20250429145730.zip" s3://adama1945-ledgerpay
 
-aws elasticbeanstalk create-application-version --application-name flaskbb --source-bundle S3Bucket="flaskbbapp",S3Key="flaskbb_deploy-$1.zip" --version-label "ver-$1" --description "file permissions" --region "us-east-2"
+aws elasticbeanstalk create-application-version --application-name flaskbb --source-bundle S3Bucket="adama1945-ledgerpay",S3Key="flaskbb_deploy-081273d-20250429145730.zip" --version-label "ver-$1" --description "file permissions" --region "us-east-2"
 
 aws elasticbeanstalk update-environment --environment-name flaskbb-environment --version-label "ver-$1" --region "us-east-2"
