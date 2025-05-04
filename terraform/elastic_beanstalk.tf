0c9372eda85fffdcaacff6d76d085283a5263eb8
gizmo1547 <120990499+gizmo1547@users.noreply.github.com>
2025-05-04 17:18:38 -0400

    Update deploy.yml and deploy.sh for ledgerpay Django application

---
+++ terraform/elastic_beanstalk.tf
@@ -0,0 +1,17 @@
+resource "aws_elastic_beanstalk_application" "application" {
+    name = "ledgerpay"  
+}
+
+resource "aws_elastic_beanstalk_environment" "environment" {
+  name                = "Django-environment"
+  cname_prefix        = "gizmo1547"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.5.1 running Python 3.11"
+
+  //platform_arn        = "arn:aws:elasticbeanstalk:us-east-1::platform/Python 3.11 running on 64bit Amazon Linux 2023/4.0.0"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+}
