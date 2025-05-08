0428ed45e9be3d753bc7305a6a12eda4b26fbddd
gizmo1547 <120990499+gizmo1547@users.noreply.github.com>
2025-05-07 20:27:51 -0400

    Add deploy script and GitHub Actions workflow for build

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Package application into a zip file
+zip -r "ledgerpay_deploy-$1.zip" ./ledgerpay manage.py requirements.txt .ebextensions
+# Adjust files above based on your application's needs
+
+# Upload zip to S3
+aws s3 cp "ledgerpay_deploy-$1.zip" s3://terraform-state-ledgerpay-gizmo1547
+
+# Create new application version
+aws elasticbeanstalk create-application-version \
+  --application-name ledgerpay \
+  --source-bundle S3Bucket="terraform-state-ledgerpay-gizmo1547",S3Key="ledgerpay_deploy-$1.zip" \
+  --version-label "ver-$1" \
+  --description "commit-sha-$1" \
+  --region us-east-1
+
+# Deploy to Elastic Beanstalk environment
+aws elasticbeanstalk update-environment \
+  --environment-name Django-environment \
+  --version-label "ver-$1" \
+  --region us-east-1
