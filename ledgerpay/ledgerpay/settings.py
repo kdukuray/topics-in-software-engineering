390d0804965a208f3d982243ecb3d5ab0268366a
kdukuray <kdukuray01@gmail.com>
2025-05-12 10:02:38 -0400

    added collect static

--- ledgerpay/ledgerpay/settings.py
+++ ledgerpay/ledgerpay/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,6 +135,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "login"
+
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Required for collectstatic
+
+# Optional but helpful on Heroku
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
