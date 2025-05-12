f3f9d269d18f3c12343196c7d80bad99dbac8a72
kdukuray <kdukuray01@gmail.com>
2025-05-12 10:07:33 -0400

    minor changes

--- ledgerpay/ledgerpay/settings.py
+++ ledgerpay/ledgerpay/settings.py
@@ -144,6 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Required for collectstati
 # Optional but helpful on Heroku
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ledgerpay', 'payments', 'static'),
 ]
 
 CORS_ALLOWED_ORIGINS = [
